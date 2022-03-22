ce73cc24e20141725f1c93e08c60f571ffbe0c5c
Jordan Rogers <j.rogers@wearedoubleline.com>
2022-03-22 13:18:13 -0500

    initial code cleanup

--- action-whitelist/src/entrypoint.ps1
+++ action-whitelist/src/entrypoint.ps1
@@ -1,65 +1,16 @@
-#param (
-#    [string] $RepoPath = "/github/workspace",
-#    [string] $whitelist = "/src/approved.json"
-#)
 Import-Module /src/action-whitelist.psm1
-function Get-LocationInfo {
-    Write-Host "Where are we? [$pwd]"
 
-    ForEach ($file in Get-ChildItem) {
-        Write-Host "- $($file.Name)"
-    }
-}
 $actionsFound = LoadAllUsedActions -RepoPath $pwd
 $unapproved = CheckIfActionsApproved -outputs $actionsFound
+$jsonObject = ($unapproved | ConvertTo-Json)
+$jsonObject | out-file ./actions.json
+Write-Output "::set-output name=actions::'$jsonObject'"
+Write-Host "Stored actions in outputs list. Use $${{ steps.<step id>.outputs.actions }} in next action to load the json"
+
 if ($unapproved.Count -gt 0) {
     Write-Error "Repo contains unapproved actions!"
-    #Write-Error $unapproved
-    # return the container with an erroneous exit code:
     exit 1
 }else{
     Write-Host "All actions were approved!"
     exit 0
 }
-<#
-function main {
-    $actionsFound = LoadAllUsedActions -RepoPath $pwd
-    $unapproved = CheckIfActionsApproved -outputs $actionsFound
-    #return $unapproved
-    #$actions = (.\action-whitelist.ps1 -RepoPath $RepoPath -whitelist $whitelist)
-    #Import-Module .\action-whitelist.psm1
-    # wite the file outside of the container so we can pick it up
-    #Write-Host "Found [$($actions.Count)] actions "
-    #Write-Verbose $actions | ConvertTo-Json -Depth 10
-    
-    #$jsonObject = ($actions | ConvertTo-Json -Depth 10 -Compress)
-    #$jsonObjectPretty = ($actions | ConvertTo-Json -Depth 10)
-    #$jsonPath = "/actions/actions.json"
-    #if (! (Test-Path $jsonPath)) {
-    #    $jsonPath = "$PSScriptRoot/actions.json"
-    #}
-    #$jsonObjectPretty | Out-File -FilePath $jsonPath
-    #Write-Output "::set-output name=actions::'$jsonObject'"
-    
-    #Write-Host "Stored actions in outputs list. Use $${{ steps.<step id>.outputs.actions }} in next action to load the json"
-}
-
-try {
-    # always run in the correct location, where our scripts are located:
-    #Set-Location $PSScriptRoot
-    
-    #Get-LocationInfo
-    # call main script:
-    main
-    
-    # return the container with the exit code = Ok:    
-    exit 0
-}
-catch {
-    # return the container with the last exit code: 
-    Write-Error "Error loading the actions:"
-    Write-Error $_
-    # return the container with an erroneous exit code:
-    exit 1
-}
-#>
--- action-whitelist/src/generic.ps1
+++ action-whitelist/src/generic.ps1
@@ -1,15 +1,6 @@
-
-#Write-Host "PSHOME: [$pshome]" 
-
-# add back the root folder to the modules path because GitHub runner seems to overwite it
 $env:PSModulePath += ":/root/.local/share/powershell/Modules"
 
-#Write-Host "PSModulePath:"
-foreach ($path in $env:PSModulePath -split ':') {
-    #Write-Host "- [$path]"
-}
 try {
-    #Write-Host "Importing module for the yaml parsing"
     Import-Module powershell-yaml -Force
 }
 catch {
