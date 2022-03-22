72247e9f6ab153f0c612f8d98e00e8941e473d84
Jordan Rogers <j.rogers@wearedoubleline.com>
2022-03-22 14:54:06 -0500

    remove some outputs

--- action-whitelist/src/entrypoint.ps1
+++ action-whitelist/src/entrypoint.ps1
@@ -5,12 +5,12 @@ $unapproved = CheckIfActionsApproved -outputs $actionsFound
 $jsonObject = ($unapproved | ConvertTo-Json)
 $jsonObject | out-file ./actions.json
 Write-Output "::set-output name=actions::'$jsonObject'"
-Write-Host "Stored actions in outputs list. Use $${{ steps.<step id>.outputs.actions }} in next action to load the json"
+#Write-Host "Stored actions in outputs list. Use $${{ steps.<step id>.outputs.actions }} in next action to load the json"
 
 if ($unapproved.Count -gt 0) {
     Write-Error "Repo contains unapproved actions!"
     exit 1
 }else{
-    Write-Host "All actions were approved!"
+    #Write-Host "All actions were approved!"
     exit 0
 }
