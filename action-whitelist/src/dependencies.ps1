b3837eb1ea6d0822779a65263f7b4d2b26864b5f
Jordan Rogers <j.rogers@wearedoubleline.com>
2022-03-22 14:39:02 -0500

    import powershell-yaml locally

--- action-whitelist/src/dependencies.ps1
+++ action-whitelist/src/dependencies.ps1
@@ -1,7 +1,7 @@
 $env:PSModulePath += ":/root/.local/share/powershell/Modules"
 
 try {
-    Import-Module powershell-yaml -Force
+    Import-Module /src/powershell-yaml/ -Force
 }
 catch {
     Write-Warning "Error during importing of the yaml module needed for parsing"
