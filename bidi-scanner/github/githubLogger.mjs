b77c751f0e59e73e8616c743a8d081a03e4e64b4
Stephen A. Fuqua <stephen@safnet.com>
2023-01-11 15:58:01 -0600

    Tune the GitHub logging

--- bidi-scanner/github/githubLogger.mjs
+++ bidi-scanner/github/githubLogger.mjs
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 // SPDX-License-Identifier: Apache-2.0
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
@@ -7,13 +8,18 @@ import core from '@actions/core';
 
 const initializeLogging = () => ({
   error: (message) => {
+    // log to plain console and create a GitHub annotation
+    console.error(message);
     core.error(message);
   },
   warn: (message) => {
+    // log to plain console and create a GitHub annotation
+    console.warn(message);
     core.warning(message);
   },
   info: (message) => {
-    core.notice(message);
+    // core.notice will output an annotation, but we just want basic console logging for this
+    console.info(message);
   },
   debug: (message) => {
     core.debug(message);
