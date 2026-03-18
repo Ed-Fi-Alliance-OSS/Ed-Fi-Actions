e9acadbb372e9259fd2424abefd7d86c01174e72
Stephen A. Fuqua <stephen@safnet.com>
2026-03-18 17:11:53 -0500

    address breaking changes from update to @actions/core

--- bidi-scanner/github/githubLogger.mjs
+++ bidi-scanner/github/githubLogger.mjs
@@ -3,25 +3,25 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-import core from '@actions/core';
+import { error as coreError, warning, debug } from '@actions/core';
 
 const initializeLogging = () => ({
   error: (message) => {
     // log to plain console and create a GitHub annotation
     console.error(message);
-    core.error(message);
+    coreError(message);
   },
   warn: (message) => {
     // log to plain console and create a GitHub annotation
     console.warn(message);
-    core.warning(message);
+    warning(message);
   },
   info: (message) => {
     // core.notice will output an annotation, but we just want basic console logging for this
     console.info(message);
   },
   debug: (message) => {
-    core.debug(message);
+    debug(message);
   },
 });
 
